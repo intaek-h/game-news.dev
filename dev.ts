90bc000b1894f5a7cfa0258b706757c1201b856f
intaekh <ahiou2ahiou@gmail.com>
2025-03-29 00:43:55 +0900

    docker deploy fix #6 remove dev.ts deno exit

--- dev.ts
+++ dev.ts
@@ -6,8 +6,3 @@ import config from "./fresh.config.ts";
 import "$std/dotenv/load.ts";
 
 await dev(import.meta.url, "./main.ts", config);
-
-// Wait for any background tasks to complete
-await new Promise((resolve) => setTimeout(resolve, 5000));
-
-Deno.exit();
