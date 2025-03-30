97cd9be874c423065826c4e359f9b71b517a0032
intaekh <ahiou2ahiou@gmail.com>
2025-03-31 00:16:41 +0900

    optimize

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,17 +1,24 @@
 import { type PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
 
 export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href={asset("/styles.css")} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Game News</title>
-        <link rel="stylesheet" href="/styles.css" />
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
-        </style>
       </head>
       <body className="mx-auto max-w-4xl">
         <Component />
