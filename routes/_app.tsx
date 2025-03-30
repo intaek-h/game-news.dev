072c1c1282c787c89fa533712e20cb64cb6a3c24
intaekh <ahiou2ahiou@gmail.com>
2025-03-31 00:34:05 +0900

    optimize css

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -13,10 +13,22 @@ export default function App({ Component }: PageProps) {
           crossorigin="true"
         />
         <link
+          rel="preload"
           href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-          rel="stylesheet"
+          as="style"
+          // @ts-expect-error: OK to ignore
+          onLoad="this.onload=null;this.rel='stylesheet'"
         />
-        <link rel="stylesheet" href={asset("/styles.css")} />
+        <link
+          rel="preload"
+          href={asset("/styles.css")}
+          as="style"
+          // @ts-expect-error: OK to ignore
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <noscript>
+          <link rel="stylesheet" href={asset("/styles.css")} />
+        </noscript>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Game News</title>
       </head>
