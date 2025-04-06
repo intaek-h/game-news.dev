a556e5006ac1344a6eb0b8327d5ad6f7231c6c8d
intaekh <ahiou2ahiou@gmail.com>
2025-04-06 20:32:16 +0900

    db replica only on main

--- db/client.ts
+++ db/client.ts
@@ -1,29 +1,26 @@
-import { createClient } from "@libsql/client/node";
+import { Config, createClient } from "@libsql/client/node";
 import { drizzle } from "drizzle-orm/libsql";
 
 const TURSO_API_KEY = Deno.env.get("TURSO_API_KEY");
 const TURSO_PRODUCTION_DB_URL = Deno.env.get("TURSO_PRODUCTION_DB_URL");
+const BRANCH = Deno.env.get("BRANCH") || "main";
 
 if (!TURSO_API_KEY || !TURSO_PRODUCTION_DB_URL) {
   throw new Error("Missing Turso database credentials in .env file");
 }
 
-export const client = createClient({
-  url: `file:${Deno.cwd()}/replica.db`,
-  syncUrl: TURSO_PRODUCTION_DB_URL,
-  authToken: TURSO_API_KEY,
-  syncInterval: 3600, // 1h
-});
+const config: Config = BRANCH === "main"
+  ? {
+    url: `file:${Deno.cwd()}/replica.db`,
+    syncUrl: TURSO_PRODUCTION_DB_URL,
+    authToken: TURSO_API_KEY,
+    syncInterval: 3600, // 1h
+  }
+  : {
+    url: TURSO_PRODUCTION_DB_URL,
+    authToken: TURSO_API_KEY,
+  };
 
-if (Deno.args.includes("sync-replica")) {
-  await client.sync();
-
-  console.info("Replica DB Synced With Production DB");
-} else {
-  console.info("Skipping Replica DB Sync");
-  console.info(
-    "To sync, run the build command with the 'sync-replica' argument",
-  );
-}
+export const client = createClient(config);
 
 export const db = drizzle({ client });
