7696ca7088be4ab0d7b9594841ff254aa6db08a2
intaekh <ahiou2ahiou@gmail.com>
2025-04-11 01:08:39 +0900

    add news pipeline to deno cron

--- cron.ts
+++ cron.ts
@@ -1,4 +1,5 @@
 import { DailyGossip } from "~/jobs/gossip/index.ts";
+import { DailyNews } from "~/jobs/news/index.ts";
 
 // 매일 UTC 10:00에 실행 (뉴욕 아침 6:00)
 Deno.cron("Article Generator", "0 10 * * *", () => {
@@ -7,6 +8,17 @@ Deno.cron("Article Generator", "0 10 * * *", () => {
   console.info(
     "\x1b[32m",
     `====================\nGossip Pipeline Running\n====================\n`,
+    "\x1b[0m",
+  );
+});
+
+// 매일 UTC 9:00에 실행 (뉴욕 아침 5:00)
+Deno.cron("Article Generator", "0 9 * * *", () => {
+  DailyNews.NewsPipeline();
+
+  console.info(
+    "\x1b[32m",
+    `====================\nNews Pipeline Running\n====================\n`,
     "\x1b[0m",
   );
 });
