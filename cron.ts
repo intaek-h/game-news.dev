ca5e742c5d17955064590f0fac8c69f968340bf2
intaekh <ahiou2ahiou@gmail.com>
2025-04-13 00:49:55 +0900

    kv test 3

--- cron.ts
+++ cron.ts
@@ -6,12 +6,15 @@ const kv = await Deno.openKv();
 //   DailyNews.NewsPipeline();
 // });
 
+console.log("kv", kv);
+
 Deno.cron("kv test", "*/1 * * * *", () => {
   let count = 0;
   kv.enqueue(["kv-test", { count: count }], {
     delay: 1000 * 10,
   });
   count++;
+  console.log("count", count);
 });
 
 kv.listenQueue((msg: unknown) => {
